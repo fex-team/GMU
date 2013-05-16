96bc62bbf94740c24d58aa8026ebb4a0ab11446c
zhoumm1008 <zhoumeimei@baidu.com>
2013-05-16 11:52:46 +0800

    [bugfix] resolution for extend

---
+++ src/core/detect.js
@@ -0,0 +1,33 @@
+/**
+ * @name $.browser
+ * @desc 扩展zepto中对browser的检测
+ *
+ * **可用属性**
+ * - ***qq*** 检测qq浏览器
+ * - ***chrome*** 检测chrome浏览器
+ * - ***uc*** 检测uc浏览器
+ * - ***version*** 检测浏览器版本
+ *
+ * @example
+ * if ($.browser.qq) {      //在qq浏览器上打出此log
+ *     console.log('this is qq browser');
+ * }
+ */
+
+(function($, navigator) {
+    var ua = navigator.userAgent,
+        na = navigator.appVersion,
+        br = $.browser;
+
+    $.extend( br, {
+        qq: /qq/i.test(ua),
+        uc: /UC/i.test(ua) || /UC/i.test(na)
+    } );
+
+    br.uc = br.uc || !br.qq && !br.chrome && !br.firefox && !/safari/i.test(ua);
+
+    try {
+        br.version = br.uc ? na.match(/UC(?:Browser)?\/([\d.]+)/)[1] : br.qq ? ua.match(/MQQBrowser\/([\d.]+)/)[1] : br.version;
+    } catch (e) {}
+
+})(Zepto, navigator);
