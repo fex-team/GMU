205c8b0a352ba1aa7e0c4352993277310d19984a
liaoxuezhi <liaoxuezhi@baidu.com>
2013-05-31 14:47:49 +0800

    优化detect, 添加对baidu broswer检测

--- src/core/detect.js
+++ src/core/detect.js
@@ -6,15 +6,17 @@
  * @import zepto.js
  */
 
-(function($, navigator) {
+(function( $, navigator ) {
+    
     /**
      * @name $.browser
      * @desc 扩展zepto中对browser的检测
      *
      * **可用属性**
      * - ***qq*** 检测qq浏览器
-     * - ***uc*** 检测uc浏览器
-     * - ***version*** 检测浏览器版本
+     * - ***uc*** 检测uc浏览器, 有些老版本的uc浏览器，不带userAgent和appVersion标记，无法检测出来
+     * - ***baidu*** 检测baidu浏览器
+     * - ***version*** 浏览器版本
      *
      * @example
      * if ($.browser.qq) {      //在qq浏览器上打出此log
@@ -22,18 +24,28 @@
      * }
      */
     var ua = navigator.userAgent,
-        na = navigator.appVersion,
-        br = $.browser;
+        br = $.browser,
+        detects = {
+            qq: /MQQBrowser\/([\d.]+)/i,
+            uc: /UCBrowser\/([\d.]+)/i,
+            baidu: /baidubrowser\/.*?([\d.]+)/i
+        },
+        ret;
 
-    $.extend( br, {
-        qq: /qq/i.test(ua),
-        uc: /UC/i.test(ua) || /UC/i.test(na)
+    $.each( detects, function( i, re ) {
+        
+        if ( (ret = ua.match( re )) ) {
+            br[ i ] = true;
+            br.version = ret[ 1 ];
+
+            // 终端循环
+            return false;
+        }
     } );
 
-    br.uc = br.uc || !br.qq && !br.chrome && !br.firefox && !/safari/i.test(ua);
+    // uc还有一种规则，就是appVersion中带 Uc字符
+    if ( !br.uc && /Uc/i.test( navigator.appVersion ) ) {
+        br.uc = true;
+    }
 
-    try {
-        br.version = br.uc ? na.match(/UC(?:Browser)?\/([\d.]+)/)[1] : br.qq ? ua.match(/MQQBrowser\/([\d.]+)/)[1] : br.version;
-    } catch (e) {}
-
-})(Zepto, navigator);
+})( Zepto, navigator );
