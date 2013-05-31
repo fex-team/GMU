de5e2e3510be21fb2d6aaf1acede494737b147ff
liaoxuezhi <liaoxuezhi@baidu.com>
2013-05-31 16:22:03 +0800

    根据代码规范，调整highlight代码

--- src/core/highlight.js
+++ src/core/highlight.js
@@ -4,15 +4,27 @@
  *  @desc 点击高亮效果
  *  @import zepto.js
  */
-(function($) {
-    var actElem, inited = false, timer, cls, removeCls = function(){
-        clearTimeout(timer);
-        if(actElem && (cls = actElem.attr('highlight-cls'))){
-            actElem.removeClass(cls).attr('highlight-cls', '');
-            actElem = null;
-        }
-    };
-    $.extend($.fn, {
+(function( $ ) {
+    var inited = false,
+
+        removeClass = function() {
+            var cls;
+
+            clearTimeout( timer );
+
+            if ( actElem && (cls = actElem.attr( 'highlight-cls' )) ) {
+                actElem.removeClass( cls ).removeAttr( 'highlight-cls' );
+                actElem = null;
+            }
+        },
+
+        en = '.highlight',
+
+        actElem, 
+        timer;
+
+    $.extend( $.fn, {
+        
         /**
          * @name highlight
          * @desc 禁用掉系统的高亮，当手指移动到元素上时添加指定class，手指移开时，移除该class
@@ -22,18 +34,25 @@
          *
          * $('a').highlight();// 把所有a的自带的高亮效果去掉。
          */
-        highlight: function(className) {
-            inited = inited || !!$(document).on('touchend.highlight touchmove.highlight touchcancel.highlight', removeCls);
-            removeCls();
+        highlight: function( className ) {
+            inited = inited || !!$( document )
+                    .on( 'touchend' + en + ' touchmove' + en + ' touchcancel' +
+                    en, removeClass );
+
+            removeClass();
             return this.each(function() {
-                var $el = $(this);
-                $el.css('-webkit-tap-highlight-color', 'rgba(255,255,255,0)').off('touchstart.highlight');
-                className && $el.on('touchstart.highlight', function() {
-                    timer = setTimeout(function() {
-                        actElem = $el.attr('highlight-cls', className).addClass(className);
-                    }, 100);
-                });
+                var $el = $( this );
+
+                $el.css( '-webkit-tap-highlight-color', 'rgba(255,255,255,0)' )
+                        .off( 'touchstart' + en );
+
+                className && $el.on( 'touchstart' + en, function() {
+                    timer = setTimeout( function() {
+                        actElem = $el.attr( 'highlight-cls', className )
+                                .addClass( className );
+                    }, 100 );
+                } );
             });
         }
-    });
-})(Zepto);
+    } );
+})( Zepto );
