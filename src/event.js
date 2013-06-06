85a8f3b5ceb86640d3add7ad6f3fdf1cc6ad6c7e
liaoxuezhi <liaoxuezhi@baidu.com>
2013-06-06 10:45:39 +0800

    添加同时绑定多个event和event namespace功能

--- src/event.js
+++ src/event.js
@@ -1,18 +1,17 @@
+/**
+ * @fileoverview Event相关
+ * @import zepto.js
+ */
+// 测试用的,测试完删除
+var gmu = window.gmu || {};
+gmu.$ = Zepto;
+
+/* global gmu*/
 (function( ns, $ ) {
     $ = $ || ns.$;
 
-    var Event = function( type, props ) {
-            if( !ret instanceof Event ) {
-                return new Event( type, props );
-            }
-
-            props && $.extend( this, props );
-            this.type = type;
-
-            return this;
-        },
-
-        slice = [].slice,
+    var slice = [].slice,
+        separator = /\s+/,
 
         returnFalse = function() {
             return false;
@@ -23,6 +22,54 @@
         },
 
         api;
+
+    function eachEvent( events, callback, iterator ) {
+
+        // 不支持对象，只支持多个event用空格隔开
+        events.split( separator ).forEach(function( type ) {
+            iterator( type, callback );
+        });
+    }
+
+    // 生成匹配namespace正则
+    function matcherFor( ns ) {
+        return new RegExp( '(?:^| )' + ns.replace( ' ', ' .* ?' ) + '(?: |$)' );
+    }
+
+    // 分离event name和event namespace
+    function parse( name ) {
+        var parts = ('' + name).split( '.' );
+
+        return {
+            e: parts[ 0 ],
+            ns: parts.slice( 1 ).sort().join( ' ' )
+        };
+    }
+
+    function findHandlers( arr, name, ns, callback, context ) {
+        var matcher;
+
+        ns && (matcher = matcherFor( ns ));
+        return arr.filter(function( handler ) {
+            return handler && 
+                    (!name || handler.e === name) &&
+                    (!ns || matcher.test( handler.ns )) &&
+                    (!callback || handler.cb === callback ||
+                    handler.cb._cb === callback) &&
+                    (!context || handler.ctx === context);
+        });
+    }
+
+    function Event( type, props ) {
+        if ( !this instanceof Event ) {
+            return new Event( type, props );
+        }
+
+        props && $.extend( this, props );
+        this.type = type;
+
+        return this;
+    }
 
     Event.prototype = {
         isDefaultPrevented: returnFalse,
@@ -39,81 +86,72 @@
 
     api = {
 
-        on: function( name, callback, context ){
-            var events;
+        on: function( name, callback, context ) {
+            var me = this,
+                set;
 
-            if( !callback ) {
+            if ( !callback ) {
                 return this;
             }
 
-            this._events || (this._events = {});
+            set = this._events || (this._events = []);
             
-            events = this._events[ name ] || (this._events[ name ] = []);
+            eachEvent( name, callback, function( name, callback ) {
+                var handler = parse( name );
 
-            events.push({
-                callback: callback, 
-                context: context, 
-                ctx: context || this
-            });
+                name = handler.e;
+
+                handler.cb = callback;
+                handler.ctx = context;
+                handler.ctx2 = context || me;
+                handler.id = set.length;
+                set.push( handler );
+            } );
 
             return this;
         },
 
-        once: function( name, callback, context ) {
-            var me = this,
-                once;
+        one: function( name, callback, context ) {
+            var me = this;
 
-            if( !callback ) {
+            if ( !callback ) {
                 return this;
             }
 
-            once = function() {
-                me.off( name, once );
-                callback.apply( context || me, arguments );
-            }
+            eachEvent( name, callback, function( name, callback ) {
+                var once = function() {
+                        me.off( name, once );
+                        callback.apply( context || me, arguments );
+                    };
+                
+                once._cb = callback;
+                me.on( name, once, context );
+            } );
 
-            once._callback = callback;
-
-            return this.on( name, once, context );
+            return this;
         },
 
         off: function( name, callback, context ) {
-            var retain, 
-                events,
-                ev,
-                i,
-                len;
+            var events = this._events;
 
-            if ( !this._events ) {
+            if ( !events ) {
                 return this;
             }
 
             if ( !name && !callback && !context ) {
-                this._events = {};
+                this._events = [];
                 return this;
             }
 
-            if ( events = this._events[ name ] ) {
-                this._events[ name ] = retain = [];
+            eachEvent( name, callback, function( name, callback ) {
+                var obj = parse( name );
 
-                if ( callback || context ) {
-
-                    for ( i = 0, len = events.length; i < len; i++ ) {
-                        ev = events[ i ];
-
-                        if ( (callback && callback !== ev.callback &&
-                                callback !== ev.callback._callback) ||
-                                (context && context !== ev.context) ) {
-
-                            retain.push( ev );
-                        }
-                    }
-                }
+                findHandlers( events, obj.e, obj.ns, callback, context )
+                        .forEach(function( handler ) {
+                            delete events[ handler.id ];
+                        });
                 
-                if ( !retain.length ) {
-                    delete this._events[ name ];
-                }
-            }
+            } );
 
             return this;
         },
@@ -122,7 +160,7 @@
             var i = -1,
                 args,
                 events,
-                i,
+                stoped,
                 len,
                 ev;
 
@@ -136,16 +174,18 @@
             evt.args = args;    // handler中可以直接通过e.args获取trigger数据
             args.unshift( evt );
 
-            if ( (events = this._events[ evt.type ]) ) {
+            events = findHandlers( this._events, evt.type, '' );
+
+            if ( events ) {
                 len = events.length;
 
                 while ( ++i < len ) {
-                    if( evt.isPropagationStopped() ||  false === 
-                            (ev = events[ i ]).callback.apply( ev.ctx, args )) {
+                    if ( (stoped = evt.isPropagationStopped()) ||  false === 
+                            (ev = events[ i ]).cb.apply( ev.ctx2, args )
+                            ) {
 
                         // 如果return false则相当于stopPropagation()和preventDefault();
-                        evt.isPropagationStopped() || (evt.stopPropagation(),
-                                evt.preventDefault());
+                        stoped || (evt.stopPropagation(), evt.preventDefault());
                         break;
                     }
                 }
@@ -155,58 +195,7 @@
         }
     };
 
-    Event.make = function( target, overrides ) {
-        var override;
-
-        $.each( api, function( key, fn ){
-            override = overrides && overrides[ key ];
-
-            target[ key ] = override ? function() {
-                var _origin = this.origin,
-                    ret;
-
-                this.origin = fn;
-                ret = override.apply( this, arguments );
-
-                typeof _origin === 'undefined' ? (this.origin = _origin) :
-                        (delete this.origin);
-
-                return ret;
-            } : fn;
-        } );
-    };
-
+    // expose
     ns.Event = Event;
-
-
-
-    // 以下代码需要移至Base中
-    gmu.Event.make( gmu.Base.prototype, {
-        trigger: function( evt ) {
-            var opEvent,
-                args = [].slice.call( arguments, 1 ),
-                ev;
-
-            typeof evt === 'string' && (evt = new gmu.Event( evt ));
-            evt.args = args;
-            args.unshift( evt );
-
-            opEvent = this._options[ evt.type ];
-
-            // 如果option中的event handler return false了
-            if( opEvent && opEvent.apply( this, args ) === false ) {
-                evt.preventDefault();
-                evt.stopPropagation();
-            }
-
-            this.origin.apply( this, arguments );
-
-            // 兼容
-            ev = $.Event( evt.type );
-            this.$el.triggerHandler( ev, args );
-            ev.isDefaultPrevented() && evt.preventDefault();
-
-            return this;
-        }
-    } );
+    ns.event = api;
 })( gmu );
