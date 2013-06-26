d709047d6c6fc7d801231f524b51007847106237
liaoxuezhi <liaoxuezhi@baidu.com>
2013-06-26 20:17:58 +0800

    添加注释

--- src/extend/offset.js
+++ src/extend/offset.js
@@ -1,8 +1,12 @@
 /**
+ * 修复Zepto中offset setter bug。
+ * 比如 被定位元素满足以下条件时，会定位不正确
+ * 1. 被定位元素不是第一个节点，且prev兄弟节点中有非absolute或者fixed定位的元素
+ * 2. 被定位元素为非absolute或者fixed定位。
+ * issue: https://github.com/gmuteam/GMU/issues/101
  * @import zepto.js
  */
-(function( $ ) {    // 修复zepto的offset setter的一个bug
-
+(function( $ ) {
     var _offset = $.fn.offset;
 
     $.fn.offset = function( coord ) {
