61df3fb2917b74b4c476489e5a40b47c13625ca4
liaoxuezhi <liaoxuezhi@baidu.com>
2013-07-02 14:20:07 +0800

    Fix calendar css issue

--- _src/core/touch.js
+++ _src/core/touch.js
@@ -115,4 +115,4 @@
   ;['swipe', 'swipeLeft', 'swipeRight', 'swipeUp', 'swipeDown', 'doubleTap', 'tap', 'singleTap', 'longTap'].forEach(function(m){
     $.fn[m] = function(callback){ return this.bind(m, callback) }
   })
-})(Zepto)
+})(Zepto);
