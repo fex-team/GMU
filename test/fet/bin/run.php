897cb6fc2c34c4c664c6ddc0c823848149066ce8
jianling <jianling@jianlingtekiMacBook-Pro.local>
2013-08-06 15:13:25 +0800

    给用例页面加上viewport

--- test/fet/bin/run.php
+++ test/fet/bin/run.php
@@ -28,6 +28,7 @@ foreach ( $_GET as $key => $paras ) {
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
     <title><?php print( "run case $title" );?></title>
     <?php $c->print_js( $title , $cov ,$release); ?>
     <link rel="stylesheet" href="../lib/css/tangramtest.css">
