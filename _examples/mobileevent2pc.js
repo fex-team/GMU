54b0267bbc1e49e5e263abc8509624eb6344e8cd
jianling <jianling@jianlingtekiMacBook-Pro.local>
2013-08-07 10:40:40 +0800

    bugfix

--- _examples/mobileevent2pc.js
+++ _examples/mobileevent2pc.js
@@ -43,78 +43,22 @@
             }
             return {e:e, ns:ns};
         },
-        initNativeTouches = function(touches, target){
-            var touchlists = [];
-            var i = 0;
-            do{
-                var identifier = touches[i] && touches[i].identifier || 0;
-                var touchesPageX = touches[i] && (touches[i].pageX || touches[i].clientX) || 0;
-                var touchesPageY = touches[i] && (touches[i].pageY || touches[i].clientY) || 0;
-                var touchesScreenX = touches[i] && touches[i].screenX || 0;
-                var touchesScreenY = touches[i] && touches[i].screenY || 0;
-                touchlists[i] = document.createTouch(null, target, identifier, touchesPageX, touchesPageY, touchesScreenX, touchesScreenY);
-                i ++;
-            } while(i < touches.length);
-            return touchlists;
-        },
-        initGenericTouches = function(touchlist, touches, target){
-            var i = 0;
-            function Touch(target, identifier, touchesPageX, touchesPageY, touchesScreenX, touchesScreenY){
-                this.target = target;
-                this.identifier = identifier;
-                this.pageX = this.clientX = touchesPageX;
-                this.pageY = this.clientY= touchesPageY;
-                this.screenX = touchesScreenX;
-                this.screenY = touchesScreenY;
-            }
-            do{
-                var identifier = touches[i] && touches[i].identifier || 0;
-                var touchesPageX = touches[i] && (touches[i].pageX || touches[i].clientX) || 0;
-                var touchesPageY = touches[i] && (touches[i].pageY || touches[i].clientY) || 0;
-                var touchesScreenX = touches[i] && touches[i].screenX || 0;
-                var touchesScreenY = touches[i] && touches[i].screenY || 0;
-                touchlist[i] = new Touch(target, identifier, touchesPageX, touchesPageY, touchesScreenX, touchesScreenY);
-                i ++;
-            } while(i < touches.length);
-            touchlist.length = i;
-        },
-        createNativeTouchList = function(touches, target){       
-            if(touches){
-                var touchlist, touchlists = this.initNativeTouches(touches, target);
-                if(touchlists.length == 1)
-                    touchlist = document.createTouchList(touchlists[0]);
-                if(touchlists.length == 2)
-                    touchlist = document.createTouchList(touchlists[0], touchlists[1]);
-            }
-            else
-                var touchlist = document.createTouchList();
-            return touchlist;
-        },
-        createGenericTouchList = function(touches, target){
-            function TouchList(){
-                this.length = 0;
-                this.item = function(i) {
-                      return this[i];
-                }
-            }
-            var touchlist = new TouchList();
-            if(touches)
-                initGenericTouches(touchlist, touches, target);
-            return touchlist;
-        },
-        createTouchList = function(touches, target) {
-            return document.createTouchList ?
-                    createNativeTouchList(touches, target):
-                    createGenericTouchList(touches, target) ;
-        };
+        callbackStack = [];
 
     $.extend($.fn, {
         on: function(event, selector, callback) {
 
+            if( Object.prototype.toString.call(selector) === '[object Function]' ) {
+                callback = selector;
+                selector = null;
+            }
+
+            $.proxy(callback);
+
             var targetActived = false;
             var transedEvent = transFn(event);
 
-            var callback = function(e){
+            var _callback = function(e){
                 var options = e || {};
                 options.changedTouches = options.changedTouches || [];
                 var bubbles = typeof options.bubbles != 'undefined' ? options.bubbles : true ;
@@ -139,37 +83,53 @@
                     0, 0, clientX, clientY, ctrlKey, altKey, shiftKey, metaKey, 
                     touches, targetTouches, changedTouches, scale, rotation, relatedTarget);
                 _event.touches = [e];
-
-                // 如果touchstart没触发，touchmove(mousemove)应该不响应
+                
                 switch( e.type ){
                     case 'mousedown':
                         targetActived = true;
-                        selector.call(null, _event);
+                        callback.call(null, _event);
                         break;
                     case 'mousemove':
-                        if( !targetActived ) {
+                        if( !targetActived ) {  // 如果touchstart没触发，touchmove(mousemove)应该不响应
                             return;
                         } else {
-                            selector.call(null, _event);
+                            callback.call(null, _event);
                             break;
                         }
                     case 'mouseup':
                         targetActived = false;
-                        selector.call(null, _event);
+                        callback.call(null, _event);
                         break;
                 }
-
                 
             };
 
-            if ( Object.prototype.toString.call(selector) === '[object String]' ) {
-                return $onFn.call(this, transedEvent, selector, callback);
-            } else {
-                return $onFn.call(this, transedEvent, callback);
-            }
+            callbackStack.push({
+                'target': this,
+                'event': transedEvent,
+                'selector': selector,
+                'callback': callback,
+                '_callback': _callback
+            });
+
+            _callback._zid = callback._zid;
+            return $onFn.call(this, transedEvent, selector, _callback);
         },
         off: function(event, selector, callback) {
+
             return $offFn.call(this, transFn(event), selector, callback);
+            // var me = this;
+
+            // if( Object.prototype.toString.call(selector) === '[object Function]' ) {
+            //     callback = selector;
+            //     selector = null;
+            // }
+
+            // callbackStack.forEach(function( item, index ){
+            //     if(item.target == me && item.event == transFn(event) && item.selector == selector && item.callback == callback) {
+            //         return $offFn.call(me, transFn(event), selector, item._callback);
+            //     }
+            // });
         }
     });
 })();
