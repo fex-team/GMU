f82b06e1e34517e97e64813d7a72827da35a9bc3
liaoxuezhi <liaoxuezhi@baidu.com>
2015-01-04 10:04:04 +0800

    add whitelist to proxy.php

--- examples/data/tabs/proxy.php
+++ examples/data/tabs/proxy.php
@@ -3,6 +3,13 @@ $file = isset($_REQUEST['file'])?$_REQUEST['file']:'sample.html';
 $debug = isset($_REQUEST['debug'])?intval($_REQUEST['debug']):false;
 sleep(2);
 $file = ltrim($file, './');
+
+$whiteList = array("baike1.json", "baike2.json", "sample.html", "sample.json", "tabs.html");
+if (!in_array($file, $whiteList)) {
+    header("HTTP/1.0 404 Not Found");
+    exit(1);
+}
+
 $content = file_exists($file)?file_get_contents($file):'<p>指定的文件不存在</p>';
 $content = preg_replace_callback('/\<%=(.*?)%\>/', '_preg_callback', $content);
 echo $content;
